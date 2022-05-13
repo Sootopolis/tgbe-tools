56638e4ea7db8feacad4ce3a7e6b24dd3dca538b
Wallace <yuzhuo.w@gmail.com>
2022-05-13 11:13:50 +0100

    updated cache

--- membership.py
+++ membership.py
@@ -13,11 +13,16 @@ if response.status_code == 200:
     for category in ('weekly', 'monthly', 'all_time'):
         for entry in response.json()[category]:
             new_members[entry['username']] = entry['joined']
-    with open('membership.json', 'r') as file:
+    with open('membership.json', 'r') as membership_json:
         try:
-            old_members = json.load(file)
+            old_members = json.load(membership_json)
         except JSONDecodeError:
             old_members = {}
+    with open('lost_members.json', 'r') as lost_members_json:
+        try:
+            lost_members = json.load(lost_members_json)
+        except JSONDecodeError:
+            lost_members = {}
     came = list(new_members.items() - old_members.items())
     went = list(old_members.items() - new_members.items())
     came.sort(key=lambda x: x[1])
@@ -40,19 +45,15 @@ if response.status_code == 200:
         print('arrivals:')
         for i in range(len(came)):
             print(came[i][0], datetime.fromtimestamp(came[i][1]))
+            if came[i][0] in lost_members:
+                del lost_members[came[i][0]]
 
     if went:
         print('departures:')
         for i in range(len(went)):
-            print(went[i][0])
-        with open('lost_members.json', 'r') as lost_members_json:
-            try:
-                lost_members = json.load(lost_members_json)
-            except JSONDecodeError:
-                lost_members = {}
+            print(went[i][0], datetime.fromtimestamp(went[i][1]))
+            lost_members[went[i][0]] = went[i][1]
         with open('lost_members.json', 'w') as lost_members_json:
-            for i in range(len(went)):
-                lost_members[went[i][0]] = went[i][1]
             # lost_members = dict(sorted(lost_members.items()))
             json.dump(lost_members, lost_members_json, sort_keys=True, indent=2)
 
@@ -62,8 +63,8 @@ if response.status_code == 200:
             print(f'{changed[i][0]} is now known as {changed[i][1]}')
 
     # new_members = dict(sorted(new_members.items()))
-    with open('membership.json', 'w') as file:
-        json.dump(new_members, file, sort_keys=True, indent=2)
+    with open('membership.json', 'w') as membership_json:
+        json.dump(new_members, membership_json, sort_keys=True, indent=2)
     print(f'total members: {len(new_members)}')
 
 else:
