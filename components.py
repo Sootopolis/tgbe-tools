3adaec349f882909b4270db1a0ddba52a6075806
Wallace <yuzhuo.w@gmail.com>
2022-07-25 18:10:51 +0100

    added type_names() and print_bold()

--- components.py
+++ components.py
@@ -1,41 +1,28 @@
 def update_invited(usernames: list[str]):
     confirm = input("confirm inviting these players by typing 'Y': ")
-    if confirm == 'Y':
-        with open('invited.txt', 'r') as file:
-            invited = file.read().strip(' \n').split('\n')
+    if confirm == "Y":
+        with open("invited.txt", "r") as file:
+            invited = file.read().strip(" \n").split("\n")
         invited += usernames
         invited.sort()
-        with open('invited.txt', 'w') as file:
-            file.write('\n'.join(invited))
-        print(f'{len(usernames)} players invited')
+        with open("invited.txt", "w") as file:
+            file.write("\n".join(invited))
+        print(f"{len(usernames)} players invited")
     else:
-        print('confirmation failed. please update manually later.')
+        print("confirmation failed. please update manually later.")
 
-#
-# def get_player_id(username: str) -> int:
-#     username = username.strip(' /')
-#     link = 'https://api.chess.com/pub/player/' + username
-#     response = get(link)
-#     info = response.json()
-#     return info['player_id']
-#
-#
-# def get_members(url: str) -> dict:
-#     url = url.strip(' /')
-#     link = 'https://api.chess.com/pub' + urlparse(url).path + '/members'
-#     response = get(link)
-#     updated = int(time())
-#     members = response.json()
-#     members['members'] = []
-#     members['updated'] = updated
-#     for category in ('weekly', 'monthly', 'all_time'):
-#         members['members'] += members.pop(category)
-#     return members
-#
-#
-# def get_player_ids(members: dict) -> dict:
-#     with IncrementalBar('Progress', max=len(members['members'])) as bar:
-#         for member in members['members']:
-#             member['player_id'] = get_player_id(member['username'])
-#             bar.next()
-#     return members
+
+def print_bold(s: str, end="\n", sep=" "):
+    print("\033[1m" + s + "\033[0m", end=end, sep=sep)
+
+
+def type_names():
+    names = []
+    print("input names:")
+    while True:
+        name = input().lower().split()
+        if name:
+            names += name
+        else:
+            break
+    return names
