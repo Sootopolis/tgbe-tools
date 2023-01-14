6679d5bcca5192bad87beb0e6f23fe0f59ad4f4e
Wallace <yuzhuo.w@gmail.com>
2023-01-14 01:13:48 +0000

    imp

--- scripts/classes.py
+++ scripts/classes.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Player:
-    username: str
-    player_id: int
+
+    username: str = ""
+    player_id: int = 0
 
     def get_homepage(self):
         return f"https://www.chess.com/member/{self.username}"
@@ -50,6 +51,7 @@ class Player:
 
 @dataclass
 class Member(Player):
+
     join_time: int = 0
     is_closed: bool = False
     is_former: bool = False
@@ -87,13 +89,15 @@ class Member(Player):
 
 @dataclass
 class Candidate(Player):
-    expiring: int
-    invited: bool = False
-    joined: bool = False
+
+    expiry: int = 0
+    is_invited: bool = False
+    has_joined: bool = False
 
 
 @dataclass
 class GameResult:
+
     win = "win"
     checkmate = "checkmated"
     agreement = "agreed"
@@ -106,3 +110,58 @@ class GameResult:
     fifty = "50move"
     abandonment = "abandoned"
     timevsinsufficient = "timevsinsufficient"
+
+
+@dataclass
+class Club:
+
+    name: str = ""
+
+    def get_members(self):
+        return f"https://api.chess.com/pub/club/{self.name}/members"
+
+    def get_matches(self):
+        return f"https://api.chess.com/pub/club/{self.name}/matches"
+
+    def get_profile(self):
+        return f"https://api.chess.com/pub/club/{self.name}"
+
+
+@dataclass
+class Config:
+
+    club_name: str = ""
+    email: str = ""
+    username: str = ""
+    avoid_admins: bool = True
+    allow_repeat: bool = True
+    max_clubs: int = 32
+    min_matches_played: int = 12
+    min_matches_ongoing: int = 1
+    max_games_ongoing: int = 100
+    min_elo: int = 1000
+    max_elo: int = 2300
+    min_score_rate: float = 0.45
+    max_score_rate: float = 0.90
+    invited_expiry: int = 180
+    timeout_expiry: int = 90
+    scanned_expiry: int = 30
+    max_offline: int = 48
+    max_move_time: int = 18
+
+    def __post_init__(self):
+        self.invited_expiry *= 86400
+        self.timeout_expiry *= 86400
+        self.scanned_expiry *= 86400
+        self.max_offline *= 3600
+        self.max_move_time *= 3600
+
+    def get_request_headers(self):
+        headers = {"Accept": "application/json"}
+        if self.email:
+            headers["From"] = self.email
+        return headers
+
+
+c = Config()
+print(c.invited_expiry)
